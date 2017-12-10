0bdf383d82ff30a0f2d3e09687c7a7b3ccc396a5
aymeric-spiga <aymeric.spiga@upmc.fr>
2017-12-10 13:38:18 +0100

    a more straightforward CGI interface using ppclass and not ppplot

--- cgi-bin/maincgi.py
+++ cgi-bin/maincgi.py
@@ -5,7 +5,6 @@ import cgi, cgitb
 
 import sys
 sys.path.insert(0, "./planetoplot/modules")
-import ppplot
 from ppclass import pp
 
 # for debugging in web browser
@@ -14,56 +13,18 @@ cgitb.enable()
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
-# Get parameters from form
-ux = form.getvalue("x")
-uy = form.getvalue("y")
-uz = form.getvalue("z")
-ut = form.getvalue("t")
-
-########################################
-
-fig = ppplot.figuref(x=8,y=6)
-
-
-
-# RETRIEVE DATA
+# RETRIEVE and PLOT DATA with PPCLASS
 fifi = "./data/wrfout_d01_2024-10-04_06z00z00_zabg"
 req = pp()
 req.file = fifi
 req.var = "HGT"
-req.x = ux
-req.y = uy
-req.z = uz
-req.t = ut
-
-ff,xx,yy,zz,tt = req.getfd()
-
-#ff,xx,yy,zz,tt = pp(file=fifi,var="HGT",z=0,t=0).getfd()
-#xx = pp(file=fifi,var="XLONG",z=0,t=0).getf()
-#yy = pp(file=fifi,var="XLAT",z=0,t=0).getf()
-#uu = pp(file=fifi,var="Um",z=0,t=0).getf()
-#vv = pp(file=fifi,var="Vm",z=0,t=0).getf()
-
-# PLOT
-pl = ppplot.plot2d()
-pl.fig = fig # have to send to figure
-pl.f = ff
-pl.x = xx
-pl.y = yy
-#pl.vx = uu
-#pl.vy = vv
-pl.legend = "yorgl"
-pl.marker = None
-pl.nyticks = 20
-pl.ylabel = "YAARGL"
-#pl.proj = "laea"
-pl.make()
-########################################
-
-# create figure
-ppplot.sendagg(fig,filename='webapp.png', dpi=150)
-
-
+req.x = form.getvalue("x")
+req.y = form.getvalue("y")
+req.z = form.getvalue("z")
+req.t = form.getvalue("t")
+req.out = "agg" 
+req.filename = "webapp"
+req.getplot()
 
 
 ##### NOW WRITE THE HTML PAGE TO USER
@@ -71,7 +32,7 @@ print "Content-type:text/html;charset=utf-8\n"
 print     #Apache needs a space after content-type
 header="""<html><head><title>Mars Climate Database: The Web Interface</title></head><body>"""
 print header
-print "THIS IS A TEST!",ux,uy,uz,ut
+print "THIS IS A TEST!"
 print "<img src='../webapp.png'><br />"
 bottom = "</body></html>"
 print bottom
